ee6b9fe71e3f29e691260f367911ca0a4039b5dc
Lionel Cardenas <lionel.cardenas@mhcomm.fr>
2025-12-29 00:45:11 +0100

    test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
-const PORT = 3001
+const PORT = 3003
 
 app.use(cors())
 app.use(express.json())
